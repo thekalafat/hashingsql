b2e95db7ee83be8fcd880f06a7d300ba34b2cd8e
Berkay Kalafat <berkalafat@outlook.com>
2019-12-01 18:19:16 +0300

    Proje dosyası ekle.

---
+++ hashingsql/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace hashingsql
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.userDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.userDataSet = new hashingsql.userDataSet();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TC :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ad :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(118, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(61, 79);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(119, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(275, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 59);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 156);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(517, 263);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // userDataSetBindingSource
+            // 
+            this.userDataSetBindingSource.DataSource = this.userDataSet;
+            this.userDataSetBindingSource.Position = 0;
+            // 
+            // userDataSet
+            // 
+            this.userDataSet.DataSetName = "userDataSet";
+            this.userDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(402, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 69);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Eşle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource userDataSetBindingSource;
+        private userDataSet userDataSet;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ hashingsql/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography; //sha algoritması için gerekli
+using System.Data.SqlClient; //sql bağlantısı için gerekli
+
+
+namespace hashingsql
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-HVC54QF;Initial Catalog=user;Integrated Security=True");
+        private void Form1_Load(object sender, EventArgs e) //form yüklendiğinde veritabanındaki var olan verileri gösteriyor
+        {
+            griddoldur();
+        }
+        //veritabanı bağlantılarını sağlamak için 
+        SqlConnection con;
+        SqlDataAdapter da;
+        SqlCommand cmd;
+        DataSet ds;
+        void griddoldur() //datagridview doldurmak için veritabanını kullanıyor
+        {
+            con = new SqlConnection("Data Source=DESKTOP-HVC54QF;Initial Catalog=user;Integrated Security=True");
+            da = new SqlDataAdapter("Select *From ogrenci", con);
+            ds = new DataSet();
+            con.Open();
+            da.Fill(ds,"ogrenci");
+            dataGridView1.DataSource = ds.Tables["ogrenci"];
+            con.Close();
+        }
+         
+        private void button1_Click(object sender, EventArgs e) //ekleme butonuna basınca veritabanına hashlenmiş tcyi yazıyor
+        {
+            //sha hashing algoritmasını kullanmak için gerekli fonksiyonlar
+            SHA256 sha = new SHA256CryptoServiceProvider();
+            string Tc = textBox1.Text; //textboxtaki veriyi tc olarak al
+            string sifrelenmisVeri = Convert.ToBase64String(sha.ComputeHash(Encoding.UTF8.GetBytes(Tc))); //tc olarak aldıgın veriyi hashle
+            cmd = new SqlCommand();
+            con.Open();
+            cmd.Connection = con;           
+            cmd.CommandText = " insert into ogrenci(tc,ad) values ('" + sifrelenmisVeri + "','" + textBox2.Text + "')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            griddoldur();
+        }
+        
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsLetter(e.KeyChar))//rakam giriliyor metin girilemiyor
+                {
+                    e.Handled = true;
+                }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int karaktersayisi = textBox1.TextLength;
+            if(karaktersayisi == 12)
+            {
+                MessageBox.Show("TC No sınırına ulaştınız !","Uyarı");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SHA256 sha = new SHA256CryptoServiceProvider();
+            string Tc = textBox1.Text; //textboxtaki veriyi tc olarak al
+            string sifrelenmisVeri2 = Convert.ToBase64String(sha.ComputeHash(Encoding.UTF8.GetBytes(Tc))); //tc olarak aldıgın veriyi hashle
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select tc from ogrenci",con);
+            SqlDataReader rdr = cmd.ExecuteReader();
+            
+            List<string> tcler = new List<string>(); //tcler adında bir liste oluştur
+            while (rdr.Read())
+            {
+                tcler.Add(rdr["tc"].ToString());//veritabanındaki tcleri ekle
+            }
+            foreach (var kullaniciTc in tcler) //veritabanındaki tclerle listedeki tcleri karşılaştır
+            {
+                if (kullaniciTc == sifrelenmisVeri2)
+                {
+                    
+                    MessageBox.Show("Kayıtlı");
+                    Form2 frm = new Form2();
+                    frm.Show();
+                    break;
+                    
+
+                }
+            }
+
+
+
+
+
+
+
+            //List<Ogrenci> ogrenciler = new List<Ogrenci>();
+            //int sayac = 0;
+            //while (sayac == ogrenciler.Count)
+            //{
+            //    Ogrenci ogr = new Ogrenci();
+            //    while (rdr.Read())
+            //    {
+
+            //        ogr.tc = rdr["tc"].ToString();
+            //        foreach (var kullanıcı in ogrenciler)
+            //        {
+            //            if (sifrelenmisVeri2 == ogr.tc)
+            //            {
+            //                MessageBox.Show("Bu kişi kayıtlı");
+
+            //            }
+            //            rdr.NextResult();
+
+            //        }
+            //        sayac++;
+
+            //        break;
+            //    }
+            //}
+            con.Close();
+        }
+    }
+
+   
+}
